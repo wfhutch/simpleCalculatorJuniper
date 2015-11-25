3dde6a7cc68aee25d03721b0e281683cb3da88ba
wfhutch <wfh2727@gmail.com>
2015-11-25 11:28:42 -0600

    Added tests for Stack class

---
+++ SimpleCalculatorTests/StackTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using SimpleCalculator;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void StackEnsureICanCreateAClassInstance()
+        {
+            Stack test = new Stack();
+            Assert.IsNotNull(test);
+        }
+    }
+}
