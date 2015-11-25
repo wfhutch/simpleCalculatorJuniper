a2fbdf5c65017ad4e2468f8a1e121b96f51cef4e
wfhutch <wfh2727@gmail.com>
2015-11-25 11:18:04 -0600

    Added Evaluate class

---
+++ SimpleCalculator/Evaluate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace SimpleCalculator
+{
+    public class Evaluate
+    {
+        Parse userInput = new Parse();
+        decimal answer = 0;
+
+        public decimal calculate(int first, string op, int second)
+        {
+            if (op == "+")
+            {
+                answer = first + second;
+            }
+            else if (op == "-")
+            {
+                answer = first - second;
+            }
+            else if (op == "*")
+            {
+                answer = first * second;
+            }
+            else if (op == "/")
+            {
+                answer = decimal.Round(Convert.ToDecimal(first) / Convert.ToDecimal(second), 2);
+            }
+            else if (op == "%")
+            {
+                answer = first % second;
+            }
+            return answer;
+        }
+    }
+}
