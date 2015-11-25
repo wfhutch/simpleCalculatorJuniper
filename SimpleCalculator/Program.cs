3c0928b4c1c0beb92d119c8035cd05764d0ccc01
wfhutch <wfh2727@gmail.com>
2015-11-25 11:19:10 -0600

    Added functionality to Program class

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace SimpleCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Parse userInput = new Parse();
+            Evaluate expression = new Evaluate();
+            Stack stack = new Stack();
+
+            bool runProgram = true;
+            int counter = 0;
+
+            while (runProgram)
+            {
+                Console.Write("[" + counter + "]> ");
+
+                string response = Console.ReadLine();
+
+                if (response == "exit" || response == "quit")
+                {
+                    Console.WriteLine("Bye!!");
+                    runProgram = false;
+                    Console.ReadKey();
+                }
+                else if(response == "lastq")
+                {
+                    string last_expression = stack.LastExpression;
+                    Console.WriteLine(last_expression);
+                }
+                else if (response == "last")
+                {
+                    decimal last_answer = stack.LastAnswer;
+                    Console.WriteLine(last_answer);
+
+                }
+                else
+                {
+                    string splitResponse = userInput.formatInput(response);
+                    if (splitResponse == "Invalid Expression. Please try again!")
+                    {
+                        Console.WriteLine(splitResponse);
+                    }
+                    else
+                    {
+                        userInput.splitString(splitResponse);
+                        decimal user_answer =
+                            expression.calculate(userInput.FirstNumber, userInput.Operator, userInput.SecondNumber);
+                        counter += 1;
+                        Console.WriteLine("   " + "=" + " " + user_answer);
+                        stack.LastExpression = response;
+                        stack.LastAnswer = user_answer;
+                    }
+                }
+            }
         }
     }
 }
