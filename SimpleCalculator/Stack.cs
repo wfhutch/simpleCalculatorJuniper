6e32c7a9fdaa628dd532624abc79255777835e07
wfhutch <wfh2727@gmail.com>
2015-11-25 11:23:13 -0600

    Added Stack class

---
+++ SimpleCalculator/Stack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Stack
+    {
+        public string LastExpression { get; set; }
+        public decimal LastAnswer { get; set; }
+    }
+}
+ 
